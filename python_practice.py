33f66cb756de15d7fb2d827e611abdfe52b9f09f
pandasandeggs <theteatreehouse@gmail.com>
2019-04-19 14:48:10 -0500

    first commit

---
+++ python_practice.py
@@ -0,0 +1,19 @@
+# Codecademy Python 2 Practice
+
+# PYTHON SYNTAX
+    skill_completed = "Python Syntax"
+
+    exercises_completed = 13
+
+    #The amount of points for each exercise may change, because points don't exist yet
+    points_per_exercise = 5
+
+    point_total = 100
+
+    point_total += (exercises_completed * points_per_exercise)
+
+    point_string = "I got " + str(point_total) + " points!"
+
+    print point_string
+
+# STRINGS & CONSOLE OUTPUT
